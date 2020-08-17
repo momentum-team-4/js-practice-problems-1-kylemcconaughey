0f861a8f1add912d5ba45207f75328ec11e77627
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-08-17 12:27:29 -0400

    wrote everything except GCD

--- homework.js
+++ homework.js
@@ -1,26 +1,64 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = "Addison";
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20;
+let people = 6;
+let leftover = 20 % 6;
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+
+function greeting(name) {
+    return "Hello, " + name + "!"
+}
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
 
+function isOdd(x) {
+    if (x % 2 === 0) {
+        return false
+    } else if (x % 2 === 1) {
+        return true;
+    } else if (x % 2 === -1) {
+        return true;
+    } else {
+        return false
+    }
+};
+
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
+
+function isEven(x) {
+    if (x % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
 
+function fahrenheitToCelsius(x) {
+    x = (x - 32) / (9 / 5);
+    return x;
+}
+
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+
+function celsiusToFahrenheit(x) {
+    x = (x * 9 / 5) + 32;
+    return x;
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -29,9 +67,25 @@
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
+function fahrenheitToKelvin(x) {
+    let y = fahrenheitToCelsius(x);
+    y += 273.15;
+    return y;
+}
+
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+
+function lesser(a, b) {
+    if (a > b) {
+        return b
+    } else if (b > a) {
+        return a
+    } else {
+        return null
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -45,6 +99,20 @@
 //
 // If any other language code is used, return nothing.
 
+function multigreeting(name, lang) {
+    if (lang === "en") {
+        return "Hello, " + name + "!"
+    } else if (lang === "es") {
+        return "¡Hola, " + name + "!"
+    } else if (lang === "fr") {
+        return "Bonjour, " + name + "!"
+    } else if (lang === "eo") {
+        return "Saluton, " + name + "!"
+        // } else {
+        //     return null
+    }
+}
+
 // 10. The greatest common divisor (https://en.wikipedia.org/wiki/Greatest_common_divisor)
 // is the largest integer that, given two other integers, can be divided into them. For
 // example, the greatest common divisor of 24 and 81 is 3. The greatest common divisor of
@@ -52,8 +120,8 @@
 //
 // One method of calculating the greatest common divisor is the "binary GCD algorithm."
 // (https://en.wikipedia.org/wiki/Greatest_common_divisor#Binary_GCD_algorithm)
-// It can be written out like the following:
 //
+// It can be written out like the following:
 // Input: a, b positive integers
 // Output: The greatest common divisor, which is g * 2**d
 // d = 0
@@ -71,3 +139,13 @@
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+
+function gcd(a, b) {
+    let d = 0;
+    while ((isEven(a)) && (isEven(b))) {
+        a = a / 2;
+        b = b / 2;
+        d++;
+    }
+
+}
